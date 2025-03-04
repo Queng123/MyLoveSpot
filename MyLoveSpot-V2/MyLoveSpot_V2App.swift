e14d1af1346e807bc823c2557a53db4dff968ede
Queng123 <quentin.brejoin@gmail.com>
2025-03-04 14:38:52 -0800

    add base project

---
+++ MyLoveSpot-V2/MyLoveSpot_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyLoveSpot_V2App.swift
+//  MyLoveSpot-V2
+//
+//  Created by Quentin Brejoin on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MyLoveSpot_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
