1d06558f47e670567d2af8d32c4513361ec1bffb
Queng123 <quentin.brejoin@gmail.com>
2025-03-18 15:00:25 -0700

    add current location loader

--- MyLoveSpot-V2/MapView.swift
+++ MyLoveSpot-V2/MapView.swift
@@ -7,12 +7,14 @@
 
 import SwiftUI
 import MapKit
+import CoreLocation
 
 struct MapView: View {
     @Binding var spots: [Spots]
     @Binding var selectedSpot: Spots?
 
-    // TODO: Start with the default user location
+    @StateObject private var locationManager = LocationManager()
+    
     @State private var region = MapCameraPosition.region(MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
         span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
@@ -31,7 +33,18 @@ struct MapView: View {
                         .tag(spot)
                     }
             }
+        }.onAppear {
+            locationManager.checkLocationAuthorization()
         }
+        .onChange(of: locationManager.isAuthorized) {
+            if let coordinate = locationManager.lastKnownLocation {
+                region = .region(MKCoordinateRegion(
+                    center: coordinate,
+                    span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+                )
+            }
+        }
+
     }
 }
 
